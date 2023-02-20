917cfaa58e79081de759794dd3748a14dbb4002c
NatKhomich <yalapulya94@mail.ru>
2023-02-20 12:06:09 +0600

    first components

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,35 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <Technologies />
+        </div>
+    );
+}
+
+function Header() {
+    return (
+        <div>
+            <a>Home </a>
+            <a>News Feed </a>
+            <a>Messages </a>
+        </div>
+    )
+}
+
+function Technologies() {
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
 }
 
 export default App;
