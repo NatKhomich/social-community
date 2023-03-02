4c7a9ede2e328f83b206d466bca9c0e57e8393d7
NatKhomich <yalapulya94@mail.ru>
2023-03-02 16:39:26 +0600

    samurai 11

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import './App.css';
-import Header from "./Header";
-import Technologies from "./Technologies";
+import Header from "./Components/Header";
+import Navbar from "./Components/Navbar";
+
+
 
 function App() {
     return (
-        <div>
-            <Header />
-            <Technologies />
+        <div className={'app-wrapper'}>
+            <Header/>
+            <Navbar />
+            <div className={'content'}>
+                <div>
+                    <img src={'https://live.staticflickr.com/65535/49704121871_d4b10262bf_b.jpg'}/>
+                </div>
+                <div>
+                    ava+description
+                    {/* <img src={'https://avatars.mds.yandex.net/i?id=e67c20f98bdc512c5d3bc20c140f8fac-5719595-images-taas-consumers&n=27&h=480&w=480'} />*/}
+
+                </div>
+                <div>
+                    My posts
+                    <div>New post</div>
+                </div>
+                <div>
+                    <div>Post 1</div>
+                    <div>Post 2</div>
+                </div>
+
+            </div>
+
         </div>
     );
 }
---
+++ src/Components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className={'header'}>
+            <img src={'https://media.istockphoto.com/id/1045368942/vector/abstract-green-leaf-logo-icon-vector-design-ecology-icon-set-eco-icon.jpg?s=612x612&w=0&k=20&c=XIfHMI8r1G73blCpCBFmLIxCtOLx8qX0O3mZC9csRLs='}/>
+        </header>
+    );
+};
+
+export default Header;
