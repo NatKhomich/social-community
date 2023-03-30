be9ff70466ee38919270ea53b7a1419c732a47a3
NatKhomich <yalapulya94@mail.ru>
2023-03-30 21:00:31 +0600

    new styled ProfileInfo

--- src/Components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/Components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
 import photo_profile from '../../../image/profile_fon.png'
+import avatar_description from '../../../image/avatar_description.jpg'
 
 const ProfileInfo = () => {
     return (
         <div>
-            <div className={s.profile_info} >
+            <div className={s.profile_info}>
                 <img src={photo_profile}/>
             </div>
-            <div className={s.description}>avatar+description</div>
+            <div className={s.description}>
+                <img src={avatar_description}/>
+                <div className={s.descriptionInfo}>
+
+                    <div className={s.name}> Natalia K. </div>
+                    <div> Date of Birth: 6 november </div>
+                    <div> City: Astana </div>
+                    <div> Instagram: @nat_khomich </div>
+                </div>
+
+            </div>
         </div>
     );
 };
