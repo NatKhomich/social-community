aea77b1be1df8361c84bef0b1d41307f3c1b6b64
NatKhomich <yalapulya94@mail.ru>
2024-01-25 16:04:03 +0600

    deploy

--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,11 @@ import {BrowserRouter, HashRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 
 ReactDOM.render(
-        <BrowserRouter>
+        <HashRouter>
             <Provider store={store}>
                 <AppContainer />
             </Provider>
-        </BrowserRouter>,
+        </HashRouter>,
         document.getElementById('root')
     );
 
